2d7bede56d864eab53c34a9bf2e803880a4ed300
Marco Godínez <markotom@gmail.com>
2013-04-27 20:21:58 -0500

    Added route to retrieve tags by each book

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,10 +1,25 @@
-define(['lib/controllers'], function (Controller) {
+define(['lib/controllers', 'db'], function (Controller, db) {
 	console.log('Api added');
-	var api = Controller({prefix : '/api'});
+	var api = Controller({prefix : '/api'})
+		,	_		= require('underscore');
 
 	api.get('/books/', function(req,res){
 		res.render('test',{ asJson : true, data : 'lol' });
 	});
+
+	api.get('/:bookId/tags', function(req, res){
+	  var tags = [];
+	  db.get(req.params.bookId, function (err, book) {
+	    if(err) throw err;
+	    db.get(book.revisionId, function (err, rev) {
+	      if(err) throw err;
+	      rev.folios.forEach(function(folio){
+	        tags = _.union(folio.tags, tags);
+	      });
+	      res.send(tags);
+	    });
+  	});
+  });
 
 	return api;
 });
