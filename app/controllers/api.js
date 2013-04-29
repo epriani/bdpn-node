b4d63672417c6187f250bf7a76dedd64adeab98e
Siedrix <Siedrix@gmail.com>
2013-04-28 20:03:12 -0400

    Deploying api for tags

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,25 +1,37 @@
 define(['lib/controllers', 'db'], function (Controller, db) {
-  console.log('Api added');
-  var api = Controller({prefix : '/api'})
-    , _   = require('underscore');
+    console.log('Api added');
+    var api = Controller({prefix : '/api'}),
+            _   = require('underscore');
 
-  api.get('/books/', function(req,res){
-    res.render('test',{ asJson : true, data : 'lol' });
-  });
-
-  api.get('/:bookId/tags', function(req, res){
-    var tags = [];
-    db.get(req.params.bookId, function (err, book) {
-      if(err) throw err;
-      db.get(book.revisionId, function (err, rev) {
-        if(err) throw err;
-        rev.folios.forEach(function(folio){
-          tags = _.union(folio.tags, tags);
-        });
-        res.send(tags);
-      });
+    api.get('/books/', function(req,res){
+        res.render('test',{ asJson : true, data : 'lol' });
     });
-  });
 
-  return api;
+    api.get('/:bookId/tags', function(req, res){
+        var tags = {};
+        db.get(req.params.bookId, function (err, book) {
+            if(err) res.send(err);
+            db.get(book.revisionId, function (err, rev) {
+                if(err) res.send(err);
+
+                rev.folios.forEach(function(folio){
+                    if(!folio.tags) return;
+
+                    folio.tags.forEach(function(item){
+                        console.log(item);
+
+                        if(!tags[item.tag]){
+                            tags[item.tag] = [];
+                        }
+
+                        tags[item.tag].push(item);
+                    });
+                });
+
+                res.send(tags);
+            });
+        });
+    });
+
+    return api;
 });
